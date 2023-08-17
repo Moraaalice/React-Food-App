f6b1ea96614fc95311ee77c4adb5a7f950579bdf
Moraaalice <alicemoraaongongo@gmail.com>
2023-08-17 20:49:49 +0300

    My react food website

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import NavBar from './NavBar';
+import Body from './Body'
+import Menu from './Menu';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Body/>
+      <Menu/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
